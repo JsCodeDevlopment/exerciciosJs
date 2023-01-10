f7f104e42b8656d0d43d7cb77bef3138a97476a0
Jonata Silva <jonatasilva118@gmail.com>
2023-01-10 14:06:55 -0300

    much bug still

---
+++ EscalacaoDeTime/Escalacao.js
@@ -0,0 +1,25 @@
+// inicialização Boas Vindas
+let welcome = alert("WELCOME GUY!\n" + "Here \you'll\ select u team.\n" + "\Let´s\ Go?!");
+
+// criando o jogador
+function createPlayer(){
+    const divDataReception = document.getElementById('dataReception');
+    const inputName = document.getElementById('name');
+    const inputPosition = document.getElementById('position');
+    const inputNumber = document.getElementById('number');
+
+    const newP = document.createElement('p');
+    newP.className = 'name';
+    newP.innerText = inputName.value;
+    divDataReception.appendChild(newP);
+
+    const newP1 = document.createElement('p');
+    newP.className = 'position';
+    newP.innerText = inputPosition.value;
+    divDataReception.appendChild(newP1);
+
+    const newP2 = document.createElement('p');
+    newP.className = 'number';
+    newP.innerText = inputNumber.value;
+    divDataReception.appendChild(newP2);
+};
