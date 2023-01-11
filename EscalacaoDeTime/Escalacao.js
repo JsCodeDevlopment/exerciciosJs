53e35008ddbd121d0799f17cde06272ba6110912
Jonata Silva <jonatasilva118@gmail.com>
2023-01-10 23:30:31 -0300

    first part of chalenge finished

--- EscalacaoDeTime/Escalacao.js
+++ EscalacaoDeTime/Escalacao.js
@@ -3,26 +3,31 @@ let welcome = alert("WELCOME GUY!\n" + "Here \you'll\ select u team.\n" + "\Let�
 
 // criando o jogador
 function createPlayer(){
+//entrada dos dados.
     const divDataReception = document.getElementById('dataReception');
     const inputName = document.getElementById('name');
     const inputPosition = document.getElementById('position');
     const inputNumber = document.getElementById('number');
-
+// criação dos elementos
     const newP = document.createElement('p');
     newP.className = 'name';
     newP.innerText = inputName.value;
-    divDataReception.appendChild(newP);
-
+    
     const newP1 = document.createElement('p');
     newP1.className = 'position';
     newP1.innerText = inputPosition.value;
-    divDataReception.appendChild(newP1);
-
+    
     const newP2 = document.createElement('p');
     newP2.className = 'number';
-    newP2.innerText = inputNumber.value;
+    newP2.innerText = inputNumber.value; 
+// atribuição da filiação
+    divDataReception.appendChild(newP);
+    divDataReception.appendChild(newP1);
     divDataReception.appendChild(newP2);
-    
+// remoção do preenchimento dos inputs
+    inputName.value = "";
+    inputPosition.value = "";
+    inputNumber.value = "";
 };
 
 // removendo o jogador
