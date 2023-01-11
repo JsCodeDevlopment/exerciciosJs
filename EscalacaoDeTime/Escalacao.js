9ec6615307eccdd6c6fadb7c07d27119a7c0a4b2
Jonata Silva <jonatasilva118@gmail.com>
2023-01-11 14:41:35 -0300

    objective finished

--- EscalacaoDeTime/Escalacao.js
+++ EscalacaoDeTime/Escalacao.js
@@ -1,37 +1,41 @@
-// inicialização Boas Vindas
+// inicialização Boas Vindas.
 let welcome = alert("WELCOME GUY!\n" + "Here \you'll\ select u team.\n" + "\Let´s\ Go?!");
 
-// criando o jogador
+// criando o jogador.
 function createPlayer(){
-//entrada dos dados.
+// entrada dos dados.
     const divDataReception = document.getElementById('dataReception');
     const inputName = document.getElementById('name');
     const inputPosition = document.getElementById('position');
     const inputNumber = document.getElementById('number');
-// criação dos elementos
+// criação dos elementos.
     const newP = document.createElement('p');
-    newP.className = 'name';
-    newP.innerText = inputName.value;
-    
-    const newP1 = document.createElement('p');
-    newP1.className = 'position';
-    newP1.innerText = inputPosition.value;
-    
-    const newP2 = document.createElement('p');
-    newP2.className = 'number';
-    newP2.innerText = inputNumber.value; 
-// atribuição da filiação
+    newP.className = 'player';
+    newP.innerText = inputName.value + " - " + inputPosition.value + " - " + inputNumber.value;
+// atribuição da filiação.
     divDataReception.appendChild(newP);
-    divDataReception.appendChild(newP1);
-    divDataReception.appendChild(newP2);
-// remoção do preenchimento dos inputs
+// apagando o conteúdo escrito no input.
     inputName.value = "";
     inputPosition.value = "";
     inputNumber.value = "";
 };
 
-// removendo o jogador
+// removendo o jogador.
 function removePlayer(){
+// entrada dos dados.
     const divDataReception = document.getElementById('dataReception');
     const inputNumberToRemove = document.getElementById('delet');
+    const pTags = divDataReception.querySelectorAll('p');
+// procura pelo n° informado no input, para apagar o jogador.
+// o if irá questionar o usuário se realmente deseja excluir o jogador.
+    if (confirm("Você tem certeza que deseja deletar o jogador?")) {
+        pTags.forEach(function(p) {
+            if (p.value = inputNumberToRemove) {
+                p.remove();
+            }
+        });
+        
+    }
+// apagando o conteúdo escrito no input.
+    inputNumberToRemove.value = "";
 };
